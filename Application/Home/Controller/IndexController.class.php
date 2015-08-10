7e531caf2503d19d8f7d7775a8cf95603623780f
1016990109 <1016990109@qq.com>
2015-08-10 16:10:33 +0800

    1

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,7 +2,12 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+	
     public function index(){
         $this->display('Index/index');
+    }
+
+        public function concrete(){
+    	$this->display('Index/article');
     }
 }
---
+++ Application/Runtime/Cache/Home/7d99a81bc0c5062f04553b059940b477.php
@@ -0,0 +1,235 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE HTML>
+<html>
+<head>
+	<title>Home</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link href="/E-LearningWeb/Application/Home/Public/css/bootstrap.css" rel='stylesheet' type='text/css' />
+	<link href="/E-LearningWeb/Application/Home/Public/css/style.css" rel='stylesheet' type='text/css' />
+	<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link href='http://fonts.useso.com/css?family=Lato:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+	<!-- 可选的Bootstrap主题文件（一般不用引入） -->
+	<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+	<!--Animation>-->
+	<script src="/E-LearningWeb/Application/Home/Public/js/wow.min.js"></script>
+	<link href="/E-LearningWeb/Application/Home/Public/css/animate.min.css" rel='stylesheet' type='text/css' />
+	<script>
+	new WOW().init();
+	</script>
+
+</head>
+<body>
+	<div class="header-home">
+		<div class="fixed-header">
+			<div class="logo wow fadeInDown" data-wow-delay="0.2s">
+				<a href="index.html">
+					<span class="secondary">大学生的知乎</span>
+					<span class="main">E-Learning</span>
+				</a>
+			</div>
+			<div class="top-nav wow bounce" data-wow-delay="0.2s">
+				<span class="menu"> </span>
+				<ul>
+					<li class="active"><a href="index.html">首页</a></li>
+					<li><a href="about.html">我的课程</a></li>
+					<li><a href="services.html">我的大学</a></li>
+					<li><a href="blog.html">讨论区</a></li>
+					<li><a href="contact.html">联系我们</a></li>		
+				</ul>
+				<!-- script-nav -->
+
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+	<!--header-->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-2">
+				<img alt="140x140" src="/E-LearningWeb/Application/Home/Public/img/magnify-alt.png" class="img-circle" />
+				<p>
+					<em>我的空间没有留下什么东西。</em>
+				</p>
+			</div>
+			<div class="col-md-8">
+				<div class="row">
+					<div class="col-md-8">
+						<div class="pagination pagination-centered">
+							<ul>
+								<li>
+									<a href="#">上一页</a>
+								</li>
+								<li>
+									<a href="#">1</a>
+								</li>
+								<li>
+									<a href="#">2</a>
+								</li>
+								<li>
+									<a href="#">3</a>
+								</li>
+								<li>
+									<a href="#">4</a>
+								</li>
+								<li>
+									<a href="#">5</a>
+								</li>
+								<li>
+									<a href="#">下一页</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-4">
+					</div>
+				</div>
+				<h3>
+					<a href="#">ThinkPHP快速入门</a>
+				</h3>
+
+				<dl class="dl-horizontal">
+					<dt>
+						Rolex
+					</dt>
+					<dd>
+						劳力士创始人为汉斯.威尔斯多夫，1908年他在瑞士将劳力士注册为商标。
+					</dd>
+					<dt>
+						Vacheron Constantin
+					</dt>
+					<dd>
+						始创于1775年的江诗丹顿已有250年历史，
+					</dd>
+					<dd>
+						是世界上历史最悠久、延续时间最长的名表之一。
+					</dd>
+					<dt>
+						IWC
+					</dt>
+					<dd>
+						创立于1868年的万国表有“机械表专家”之称。
+					</dd>
+					<dt>
+						Cartier
+					</dt>
+					<dd>
+						卡地亚拥有150多年历史，是法国珠宝金银首饰的制造名家。
+					</dd>
+				</dl>
+
+				<!-- 回复 -->
+				<hr style="height:5px;border:none;border-top:1px ridge blue;" />
+				<p>
+					本可视化布局程序在HTML5浏览器上运行更加完美, 能实现自动本地化保存, 即使关闭了网页, 下一次打开仍然能恢复上一次的操作.
+				</p>
+				<p>
+					<div style="text-align:right;width:100%;">
+						<a class="btn-inline" href="#">赞</a>
+						<a class="btn-inline" href="#">回复</a>
+					</div>
+				</p>
+				<hr style="height:5px;border:none;border-top:1px ridge blue;" />
+				<p>
+					本可视化布局程序在HTML5浏览器上运行更加完美, 能实现自动本地化保存, 即使关闭了网页, 下一次打开仍然能恢复上一次的操作.
+				</p>
+				<p>
+					<div style="text-align:right;width:100%;">
+						<a class="btn-inline" href="#">赞</a>
+						<a class="btn-inline" href="#">回复</a>
+					</div>
+				</p>
+				<hr style="height:5px;border:none;border-top:1px ridge blue;" />
+				<p>
+					本可视化布局程序在HTML5浏览器上运行更加完美, 能实现自动本地化保存, 即使关闭了网页, 下一次打开仍然能恢复上一次的操作.
+				</p>
+				<p>
+					<div style="text-align:right;width:100%;">
+						<a class="btn-inline" href="#">赞</a>
+						<a class="btn-inline" href="#">回复</a>
+					</div>
+				</p>
+				<hr style="height:5px;border:none;border-top:1px ridge blue;" />
+			</div>
+			<div class="col-md-2">
+			</div>
+		</div>
+	</div>
+	<div class="pagination pagination-centered" style=";width:100%;margin-left:70%;">
+							<ul>
+								<li>
+									<a href="#">上一页</a>
+								</li>
+								<li>
+									<a href="#">1</a>
+								</li>
+								<li>
+									<a href="#">2</a>
+								</li>
+								<li>
+									<a href="#">3</a>
+								</li>
+								<li>
+									<a href="#">4</a>
+								</li>
+								<li>
+									<a href="#">5</a>
+								</li>
+								<li>
+									<a href="#">下一页</a>
+								</li>
+							</ul>
+						</div>
+	<div class="footer wow fadeInUp" data-wow-delay="0.4s">
+		<div class="container">
+			<div class="footer_top">
+				<div class="col-sm-3">
+					<ul class="list1">
+						<h3>浏览</h3>
+						<li><a href="#">我的大学</a></li>
+						<li><a href="#">我的课程</a></li>
+						<li><a href="#">讨论区</a></li>
+						<li><a href="#">联系我们</a></li>
+					</ul>
+				</div>
+				<div class="col-sm-3">
+					<ul class="list1">
+						<h3>下个功能等你来猜</h3>
+						<li><a href="#">博客</a></li>
+						<li><a href="#">管理</a></li>
+						<li><a href="#">历史</a></li>
+						<li><a href="#">文学</a></li>
+					</ul>
+				</div>
+				<div class="col-sm-3">
+					<ul class="list1">
+						<h3>关于学习</h3>
+						<li><a href="#">关于我们</a></li>
+						<li><a href="#">应用</a></li>
+						<li><a href="#">学习环境</a></li>
+						<li><a href="#">注册</a></li>
+					</ul>
+				</div>
+				<div class="col-sm-3">
+					<ul class="socials">
+						<li><a href="#"><i class="fa fb fa-facebook"></i></a></li>
+						<li><a href="#"><i class="fa tw fa-twitter"></i></a></li>
+					</ul>
+					<ul class="list2">
+						<li><strong class="phone">+8618668775879</strong><br><small>周一到周五 / 9.00AM - 06.00PM</small></li>
+						<li>Questions? <a href="malito:mail@demolink.org">hcw13@software.nju.edu.cn</a></li>
+					</ul>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<div class="copy">
+		<p>Copyright &copy; 2015.E-Learning团队。</p>
+	</div>
+	<div style="display:none"><script src='http://v7.cnzz.com/stat.php?id=155540&web_id=155540' language='JavaScript' charset='gb2312'></script></div>
+	<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+	<script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+
+	<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+	<script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
