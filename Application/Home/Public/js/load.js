9536d8b5ddf29b0fbe987fd2ed17847102c2d303
1016990109 <1016990109@qq.com>
2015-08-14 20:04:40 +0800

    login

--- Application/Home/Public/js/load.js
+++ Application/Home/Public/js/load.js
@@ -5,4 +5,10 @@ $(function(){
     $('#department-more').click(function(event){
         $('.department-list').append("<li><a href='#'><h4>政府管理学院</h4></a></li>");
     });
+
+    $('#article-more').click(function(event){
+        $('.topic-list').append(" <li><div class='topic-head'><h3 align='left'>E-Learning</h3><div class='topic-meta'><span class='author'><a href='#'>gaoyang</a></span> &nbsp;&nbsp;&nbsp;<span>软件学院</span> &nbsp;&nbsp;&nbsp; <span>大二</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span>2015年8月15日</span> </div> </div> <hr> <div class='topic-content'> <p>为什么曹二这么蠢这么蠢这么蠢这么蠢这么蠢这么蠢这么蠢这么蠢这么蠢这么蠢这么蠢</p> </div> <div class='topic-readmore-btn'> <ul class='list-inline'> <li> <a href='#'> <i title='' class='icon icon-agree' data-original-title='赞同回复'data-toggle='tooltip' data-placement='right'></i> <b class='count'>50</b> </a> </li> <li> <a href='#'> <i title='' class='icon icon-comment' data-original-title='赞同回复'data-toggle='tooltip' data-placement='right'></i> <b class='count'>100</b> </a> </li> <a class='btn btn-default btn-nav' href='{:U('Article/index')}'>阅读全文</a> </ul> </div> </li>");
+    });
+
+
 });
