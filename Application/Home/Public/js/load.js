10c9d5aadfbb70ebcadb711737f5b3bc3af5d19d
1016990109 <1016990109@qq.com>
2015-08-14 18:18:11 +0800

    summery

---
+++ Application/Home/Public/js/load.js
@@ -0,0 +1,8 @@
+/**
+ * Created by HCW on 2015/8/14.
+ */
+$(function(){
+    $('#department-more').click(function(event){
+        $('.department-list').append("<li><a href='#'><h4>政府管理学院</h4></a></li>");
+    });
+});
